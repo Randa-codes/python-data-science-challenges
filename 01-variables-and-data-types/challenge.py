cc30b7f14fd455256dbbcd603f046f7ce9a81f04
Randa Hanadi Boukhenchouche <renda.boukhenchouche@gmail.com>
2025-04-19 13:23:44 -0300

    Add my version of the variables and data types challenge

---
+++ 01-variables-and-data-types/challenge.py
@@ -0,0 +1,9 @@
+# challenge.py
+
+# Mini Challenge: Variables & Data Types
+
+name = 'Randa'
+age = 21
+height = 1.65
+
+print('My name is: ' + name, 'I am: ' + str(age), 'My height is:', str(height))
