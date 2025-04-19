e6b099e530fbf51cf2a0f6bd2cddd5d131b5547e
Randa Hanadi Boukhenchouche <renda.boukhenchouche@gmail.com>
2025-04-19 13:43:14 -0300

    Add my version of rectangle area & perimeter challenge

---
+++ 02-expressions-and-operators/rectangle_area.py
@@ -0,0 +1,16 @@
+# rectangle_area.py
+
+# 💡 Mini Challenge: Expressions & Operators
+# Calculate the area and perimeter of a rectangle
+
+# Define dimensions
+width = 3
+height = 7
+
+# Calculate area
+rectangle_area = width * height
+print("📐 Area of the rectangle is:", rectangle_area)
+
+# Calculate perimeter
+rectangle_perimeter = 2 * (width + height)
+print("📏 Perimeter of the rectangle is:", rectangle_perimeter)
